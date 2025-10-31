4dd1b81fae555b54476eebf89e33dd5784ca3c34
arturmnc <artur.mnc@gmail.com>
2025-10-31 20:05:35 -0300

    testing

--- war.c
+++ war.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <locale.h>
 // ============================================================================
 //         PROJETO WAR ESTRUTURADO - DESAFIO DE CÓDIGO
 // ============================================================================
